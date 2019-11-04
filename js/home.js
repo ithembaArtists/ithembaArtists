e6372c7f88329dcc17b027cabfcfcf17ddead651
Max Bye <ithembaartists@gmail.com>
2019-11-04 21:12:12 +0000

    populated nav

---
+++ js/home.js
@@ -0,0 +1,122 @@
+/*
+* POPULATE
+*/
+var sidebarNames = [];
+var sidebarDates = [];
+var sidebarAge = [];
+var sidebarOccassions = [];
+var sidebarSizes = [];
+function populate() {
+  sidebarNames = [];
+  sidebarDates = [];
+  sidebarAge = [];
+  sidebarOccassions = [];
+  sidebarSizes = [];
+  images.forEach(intPopulate);
+}
+
+function intPopulate(image, index) {
+
+  // populates gallery
+  $('.thumbnailGallery').append("\
+    <div class='col-sm-4 mt-5 thumbnail blue new'>\
+      <p>new</p>\
+      <img \
+        src='img/" + image.month + image.year + "/" + image.src + "' \
+        name='" + image.name + "' \
+        date='" + image.date + "' \
+        size='" + image.size + "'            \
+        alt='" + image.name + "'s art' \
+      > \
+    </div>");
+
+    // populate sidebar
+    // NAMES
+    if( sidebarNames.indexOf(image.name) === -1 ) {
+      sidebarNames.push(image.name);
+    }
+    console.log('sidebarNames',sidebarNames);
+    // names
+    // OCCASSION
+    if( sidebarOccassions.indexOf(image.occassion) === -1 ) {
+      sidebarOccassions.push(image.occassion);
+    }
+    console.log('sidebarOccassions',sidebarOccassions);
+    // occassion
+    // SIZE
+    if( sidebarSizes.indexOf(image.size) === -1 ) {
+      sidebarSizes.push(image.size);
+    }
+    console.log('sidebarSizes',sidebarSizes);
+    // size
+    // DATE
+    if( sidebarDates.indexOf(image.date) === -1 ) {
+      sidebarDates.push(image.date);
+    }
+    console.log('sidebarDates',sidebarDates);
+    // date
+    // AGE
+    if( sidebarAge.indexOf(image.age) === -1 ) {
+      sidebarAge.push(image.age);
+    }
+    console.log('sidebarAge',sidebarAge);
+    // age
+ 
+}
+
+populate();
+// populate
+
+/*
+* SEARCH
+*/
+function findObjectByKey(array, key, value) {
+  for (var i = 0; i < array.length; i++) {
+      if (array[i][key].includes(value)) {
+        searchResults.push(array[i]);
+      }
+  }
+  return null;
+}
+
+var searchResults = [];
+function search(val){
+  searchResults = [];
+  findObjectByKey(images, 'occassion', val);
+  findObjectByKey(images, 'size', val);
+  findObjectByKey(images, 'name', val);
+  findObjectByKey(images, 'year', val);
+  console.log('searchResults', searchResults);
+}
+
+search('September 2019');
+// search
+
+/* 
+* CONVERT MONTH
+*/
+// function intToMonth(int){
+//   // convert months
+//   const monthNames = [
+//     "January", "February", "March", "April", "May", "June",
+//     "July", "August", "September", "October", "November", "December"
+//   ];
+
+//   var months = {
+//     '01' : "January",
+//     '02' : "February",
+//     '03' : "March",
+//     '04' : "April",
+//     '05' : "May",
+//     '06' : "June",
+//     '07' : "July",
+//     '08' : "August",
+//     '09' : "September",
+//     '10' : "October",
+//     '11' : "November",
+//     '12' : "December"
+//   }
+
+//   return months[int];
+// }
+// convert month
---
+++ js/images.js
@@ -0,0 +1,10 @@
+var images = [
+  {'month':'09', 'year': '2019', 'date':'September 2019', 'src':'1.png', 'size':'card', 'occassion':'christmas', 'name':'chris', 'age':'3'},
+  {'month':'09', 'year': '2019', 'date':'September 2019', 'src':'2.png', 'size':'card','occassion':'christmas', 'name':'evan', 'age':'3'},
+  {'month':'09', 'year': '2019', 'date':'September 2019', 'src':'3.png', 'size':'card','occassion':'christmas', 'name':'luke', 'age':'3'},
+  {'month':'09', 'year': '2019', 'date':'September 2019', 'src':'4.png', 'size':'large', 'occassion':'painting', 'name':'micheline', 'age':'3'},
+  {'month':'09', 'year': '2019', 'date':'September 2019', 'src':'5.png', 'size':'large', 'occassion':'painting', 'name':'ondela', 'age':'3'},
+  {'month':'09', 'year': '2019', 'date':'September 2019', 'src':'6.png', 'size':'small', 'occassion':'painting', 'name':'ronell', 'age':'4'},
+  {'month':'09', 'year': '2019', 'date':'September 2019', 'src':'7.png', 'size':'small', 'occassion':'painting', 'name':'konaldo', 'age':'5'}
+];
+
